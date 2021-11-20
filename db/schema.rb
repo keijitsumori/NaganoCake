6079903ff43b0d1e945573d630a32199d4711b5e
keijitsumori <kjmikann3035@docomo.ne.jp>
2021-11-20 07:37:31 +0000

    make genre model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_20_073136) do
+ActiveRecord::Schema.define(version: 2021_11_20_073625) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -59,6 +59,12 @@ ActiveRecord::Schema.define(version: 2021_11_20_073136) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
