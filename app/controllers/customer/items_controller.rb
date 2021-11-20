c63949bfbb30caf162566332a4d438b95c1b6bf8
keijitsumori <kjmikann3035@docomo.ne.jp>
2021-11-20 14:32:40 +0000

    make item system

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -1,2 +1,13 @@
 class Customer::ItemsController < ApplicationController
+    def index
+        @genres = Genre.all
+        @items = Item.all
+        @items = @items.where(genre_id: params[:genre_id].to_i) if params[:genre_id].present?
+    end
+
+    def show
+        @genres = Genre.all
+        @item = Item.find(params[:id])
+        @cart_item = CartItem.new
+    end
 end
