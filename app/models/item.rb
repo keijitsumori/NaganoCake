88dc2b19442ec6abd40395ea24e1ddec8207c170
keijitsumori <kjmikann3035@docomo.ne.jp>
2021-11-22 03:37:49 +0000

    create tax price system

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
     attachment :image
+    
+    has_many :cart_items
+    has_many :order_details
+    belongs_to :genre
+
+  def with_tax_price
+    (price * 1.1).floor
+  end
+
 end
