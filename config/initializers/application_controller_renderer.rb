42156842231f3a61e70869cc76eb3dfdb1d1a870
keijitsumori <kjmikann3035@docomo.ne.jp>
2021-11-22 03:29:40 +0000

    add refile key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd1a7f7fb3df1b69d22750eb779af05b1ae6dcf3f31483e5ce2a9d000aff47f87c3d98c80a8670aab0fefcdd73dbfafd4bf5a309944194efeac03d184d788de79'
